600bd0dacd1720681c957e51da005de0e0c39fc8
Aleena Sikandar <Aleena Sikandar@DESKTOP-9T6DO7L>
2026-08-19 17:30:54 +0500

    db backup req form ui logic implemented

--- LoginForm/WinFormsApp1/Form2.Designer.cs
+++ LoginForm/WinFormsApp1/Form2.Designer.cs
@@ -28,89 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            tabControl1 = new TabControl();
-            tabPage1 = new TabPage();
-            tabPage2 = new TabPage();
-            button2 = new Button();
-            tabControl1.SuspendLayout();
-            tabPage1.SuspendLayout();
-            tabPage2.SuspendLayout();
+            bckp_rq_lgout_btn = new Button();
+            tabControl = new TabControl();
+            bckp_rq_tbPg = new TabPage();
+            cmpnydb_lbl = new Label();
+            bckp_hstry_tbPg = new TabPage();
+            cmpnydb_cmbBx = new ComboBox();
+            bckp_hstry_lgout_btn = new Button();
+            rstr_rq_nm_lbl = new Label();
+            rstr_rq_nm_txtbx = new TextBox();
+            bckp_src_lbl = new Label();
+            frsh_bckp_rdBtn = new RadioButton();
+            lst_bckp_rdBtn = new RadioButton();
+            othr_rdBtn = new RadioButton();
+            bckp_dt_pckr = new DateTimePicker();
+            sbmt_btn = new Button();
+            cncl_btn = new Button();
+            tabControl.SuspendLayout();
+            bckp_rq_tbPg.SuspendLayout();
+            bckp_hstry_tbPg.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // bckp_rq_lgout_btn
             // 
-            button1.FlatAppearance.BorderSize = 0;
-            button1.Location = new Point(831, 22);
-            button1.Name = "button1";
-            button1.Size = new Size(188, 30);
-            button1.TabIndex = 4;
-            button1.Text = "Log Out";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            bckp_rq_lgout_btn.BackColor = Color.Red;
+            bckp_rq_lgout_btn.FlatAppearance.BorderSize = 0;
+            bckp_rq_lgout_btn.FlatStyle = FlatStyle.Flat;
+            bckp_rq_lgout_btn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bckp_rq_lgout_btn.ForeColor = SystemColors.ControlLightLight;
+            bckp_rq_lgout_btn.Location = new Point(296, 336);
+            bckp_rq_lgout_btn.Name = "bckp_rq_lgout_btn";
+            bckp_rq_lgout_btn.Size = new Size(94, 29);
+            bckp_rq_lgout_btn.TabIndex = 4;
+            bckp_rq_lgout_btn.Text = "Log Out";
+            bckp_rq_lgout_btn.UseVisualStyleBackColor = false;
+            bckp_rq_lgout_btn.Click += bckp_rq_lgout_btn_Click;
             // 
-            // tabControl1
+            // tabControl
             // 
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 12);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1048, 517);
-            tabControl1.TabIndex = 5;
+            tabControl.Controls.Add(bckp_rq_tbPg);
+            tabControl.Controls.Add(bckp_hstry_tbPg);
+            tabControl.Location = new Point(12, 12);
+            tabControl.Name = "tabControl";
+            tabControl.SelectedIndex = 0;
+            tabControl.Size = new Size(425, 428);
+            tabControl.TabIndex = 5;
             // 
-            // tabPage1
+            // bckp_rq_tbPg
             // 
-            tabPage1.Controls.Add(button1);
-            tabPage1.Location = new Point(4, 29);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1040, 484);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "New Backup Request";
-            tabPage1.UseVisualStyleBackColor = true;
+            bckp_rq_tbPg.BackColor = SystemColors.Control;
+            bckp_rq_tbPg.Controls.Add(cncl_btn);
+            bckp_rq_tbPg.Controls.Add(sbmt_btn);
+            bckp_rq_tbPg.Controls.Add(bckp_dt_pckr);
+            bckp_rq_tbPg.Controls.Add(othr_rdBtn);
+            bckp_rq_tbPg.Controls.Add(lst_bckp_rdBtn);
+            bckp_rq_tbPg.Controls.Add(frsh_bckp_rdBtn);
+            bckp_rq_tbPg.Controls.Add(bckp_src_lbl);
+            bckp_rq_tbPg.Controls.Add(rstr_rq_nm_txtbx);
+            bckp_rq_tbPg.Controls.Add(rstr_rq_nm_lbl);
+            bckp_rq_tbPg.Controls.Add(cmpnydb_cmbBx);
+            bckp_rq_tbPg.Controls.Add(cmpnydb_lbl);
+            bckp_rq_tbPg.Controls.Add(bckp_rq_lgout_btn);
+            bckp_rq_tbPg.Location = new Point(4, 29);
+            bckp_rq_tbPg.Name = "bckp_rq_tbPg";
+            bckp_rq_tbPg.Padding = new Padding(3);
+            bckp_rq_tbPg.Size = new Size(417, 395);
+            bckp_rq_tbPg.TabIndex = 0;
+            bckp_rq_tbPg.Text = "New Backup Request";
             // 
-            // tabPage2
+            // cmpnydb_lbl
             // 
-            tabPage2.Controls.Add(button2);
-            tabPage2.Location = new Point(4, 29);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1040, 484);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            cmpnydb_lbl.AutoSize = true;
+            cmpnydb_lbl.Location = new Point(22, 22);
+            cmpnydb_lbl.Name = "cmpnydb_lbl";
+            cmpnydb_lbl.Size = new Size(140, 20);
+            cmpnydb_lbl.TabIndex = 5;
+            cmpnydb_lbl.Text = "Company database:";
             // 
-            // button2
+            // bckp_hstry_tbPg
             // 
-            button2.FlatAppearance.BorderSize = 0;
-            button2.Location = new Point(836, 19);
-            button2.Name = "button2";
-            button2.Size = new Size(188, 30);
-            button2.TabIndex = 5;
-            button2.Text = "Log Out";
-            button2.UseVisualStyleBackColor = true;
+            bckp_hstry_tbPg.Controls.Add(bckp_hstry_lgout_btn);
+            bckp_hstry_tbPg.Location = new Point(4, 29);
+            bckp_hstry_tbPg.Name = "bckp_hstry_tbPg";
+            bckp_hstry_tbPg.Padding = new Padding(3);
+            bckp_hstry_tbPg.Size = new Size(1040, 484);
+            bckp_hstry_tbPg.TabIndex = 1;
+            bckp_hstry_tbPg.Text = "tabPage2";
+            bckp_hstry_tbPg.UseVisualStyleBackColor = true;
+            // 
+            // cmpnydb_cmbBx
+            // 
+            cmpnydb_cmbBx.FlatStyle = FlatStyle.Flat;
+            cmpnydb_cmbBx.FormattingEnabled = true;
+            cmpnydb_cmbBx.Location = new Point(22, 54);
+            cmpnydb_cmbBx.Name = "cmpnydb_cmbBx";
+            cmpnydb_cmbBx.Size = new Size(368, 28);
+            cmpnydb_cmbBx.TabIndex = 6;
+            cmpnydb_cmbBx.Text = "Select Database...";
+            // 
+            // bckp_hstry_lgout_btn
+            // 
+            bckp_hstry_lgout_btn.BackColor = Color.Red;
+            bckp_hstry_lgout_btn.FlatAppearance.BorderSize = 0;
+            bckp_hstry_lgout_btn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bckp_hstry_lgout_btn.ForeColor = SystemColors.ControlLightLight;
+            bckp_hstry_lgout_btn.Location = new Point(829, 22);
+            bckp_hstry_lgout_btn.Name = "bckp_hstry_lgout_btn";
+            bckp_hstry_lgout_btn.Size = new Size(188, 30);
+            bckp_hstry_lgout_btn.TabIndex = 6;
+            bckp_hstry_lgout_btn.Text = "Log Out";
+            bckp_hstry_lgout_btn.UseVisualStyleBackColor = false;
+            // 
+            // rstr_rq_nm_lbl
+            // 
+            rstr_rq_nm_lbl.AutoSize = true;
+            rstr_rq_nm_lbl.Location = new Point(22, 111);
+            rstr_rq_nm_lbl.Name = "rstr_rq_nm_lbl";
+            rstr_rq_nm_lbl.Size = new Size(156, 20);
+            rstr_rq_nm_lbl.TabIndex = 7;
+            rstr_rq_nm_lbl.Text = "Restore request name:";
+            //rstr_rq_nm_lbl.Click += this.label2_Click;
+            // 
+            // rstr_rq_nm_txtbx
+            // 
+            rstr_rq_nm_txtbx.BorderStyle = BorderStyle.None;
+            rstr_rq_nm_txtbx.Location = new Point(22, 143);
+            rstr_rq_nm_txtbx.Name = "rstr_rq_nm_txtbx";
+            rstr_rq_nm_txtbx.Size = new Size(368, 20);
+            rstr_rq_nm_txtbx.TabIndex = 9;
+            // 
+            // bckp_src_lbl
+            // 
+            bckp_src_lbl.AutoSize = true;
+            bckp_src_lbl.Location = new Point(22, 194);
+            bckp_src_lbl.Name = "bckp_src_lbl";
+            bckp_src_lbl.Size = new Size(107, 20);
+            bckp_src_lbl.TabIndex = 10;
+            bckp_src_lbl.Text = "Backup source:";
+            // 
+            // frsh_bckp_rdBtn
+            // 
+            frsh_bckp_rdBtn.AutoSize = true;
+            frsh_bckp_rdBtn.Location = new Point(22, 226);
+            frsh_bckp_rdBtn.Name = "frsh_bckp_rdBtn";
+            frsh_bckp_rdBtn.Size = new Size(116, 24);
+            frsh_bckp_rdBtn.TabIndex = 12;
+            frsh_bckp_rdBtn.TabStop = true;
+            frsh_bckp_rdBtn.Text = "Fresh backup";
+            frsh_bckp_rdBtn.UseVisualStyleBackColor = true;
+            // 
+            // lst_bckp_rdBtn
+            // 
+            lst_bckp_rdBtn.AutoSize = true;
+            lst_bckp_rdBtn.Location = new Point(144, 226);
+            lst_bckp_rdBtn.Name = "lst_bckp_rdBtn";
+            lst_bckp_rdBtn.Size = new Size(108, 24);
+            lst_bckp_rdBtn.TabIndex = 13;
+            lst_bckp_rdBtn.TabStop = true;
+            lst_bckp_rdBtn.Text = "Last backup";
+            lst_bckp_rdBtn.UseVisualStyleBackColor = true;
+            // 
+            // othr_rdBtn
+            // 
+            othr_rdBtn.AutoSize = true;
+            othr_rdBtn.Location = new Point(258, 226);
+            othr_rdBtn.Name = "othr_rdBtn";
+            othr_rdBtn.Size = new Size(67, 24);
+            othr_rdBtn.TabIndex = 14;
+            othr_rdBtn.TabStop = true;
+            othr_rdBtn.Text = "Other";
+            othr_rdBtn.UseVisualStyleBackColor = true;
+            // 
+            // bckp_dt_pckr
+            // 
+            bckp_dt_pckr.Enabled = false;
+            bckp_dt_pckr.Location = new Point(22, 276);
+            bckp_dt_pckr.Name = "bckp_dt_pckr";
+            bckp_dt_pckr.Size = new Size(303, 27);
+            bckp_dt_pckr.TabIndex = 15;
+            // 
+            // sbmt_btn
+            // 
+            sbmt_btn.BackColor = SystemColors.Desktop;
+            sbmt_btn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            sbmt_btn.ForeColor = SystemColors.Control;
+            sbmt_btn.Location = new Point(22, 336);
+            sbmt_btn.Name = "sbmt_btn";
+            sbmt_btn.Size = new Size(94, 29);
+            sbmt_btn.TabIndex = 16;
+            sbmt_btn.Text = "Submit";
+            sbmt_btn.UseVisualStyleBackColor = false;
+            // 
+            // cncl_btn
+            // 
+            cncl_btn.BackColor = SystemColors.ActiveCaption;
+            cncl_btn.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cncl_btn.ForeColor = SystemColors.Control;
+            cncl_btn.Location = new Point(158, 336);
+            cncl_btn.Name = "cncl_btn";
+            cncl_btn.Size = new Size(94, 29);
+            cncl_btn.TabIndex = 17;
+            cncl_btn.Text = "Cancel";
+            cncl_btn.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1072, 541);
-            Controls.Add(tabControl1);
+            ClientSize = new Size(449, 451);
+            Controls.Add(tabControl);
             Name = "Form2";
             Padding = new Padding(20);
             Text = "Form2";
-            tabControl1.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
-            tabPage2.ResumeLayout(false);
+            tabControl.ResumeLayout(false);
+            bckp_rq_tbPg.ResumeLayout(false);
+            bckp_rq_tbPg.PerformLayout();
+            bckp_hstry_tbPg.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-        private Button button1;
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
-        private Button button2;
+        private Button bckp_rq_lgout_btn;
+        private TabControl tabControl;
+        private TabPage bckp_rq_tbPg;
+        private TabPage bckp_hstry_tbPg;
+        private Label cmpnydb_lbl;
+        private ComboBox cmpnydb_cmbBx;
+        private Button bckp_hstry_lgout_btn;
+        private Label rstr_rq_nm_lbl;
+        private TextBox rstr_rq_nm_txtbx;
+        private RadioButton lst_bckp_rdBtn;
+        private RadioButton frsh_bckp_rdBtn;
+        private Label bckp_src_lbl;
+        private RadioButton othr_rdBtn;
+        private DateTimePicker bckp_dt_pckr;
+        private Button cncl_btn;
+        private Button sbmt_btn;
     }
 }
--- LoginForm/WinFormsApp1/Form2.cs
+++ LoginForm/WinFormsApp1/Form2.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
@@ -15,26 +8,190 @@ namespace WinFormsApp1
         public Form2()
         {
             InitializeComponent();
+            bckp_hstry_tbPg.Text = "Backup History";
+            cmpnydb_lbl.Text = "Company database:";
 
-            tabPage2.Text = "Backup History";
+            //dummy databases for now, later, these can come from your backend/API.
+            cmpnydb_cmbBx.Items.Add("dummyclient1");
+            cmpnydb_cmbBx.Items.Add("dummyclient2");
+            cmpnydb_cmbBx.Items.Add("dummyclient3");
+            cmpnydb_cmbBx.DropDownStyle = ComboBoxStyle.DropDownList;
 
-            // Set the welcome message
-            //lblWelcome.Text = "Welcome to the devOps page!";
+            //initially disabled until other radio buton selected
+            bckp_dt_pckr.Enabled = false;
+
+            frsh_bckp_rdBtn.CheckedChanged +=
+                BackupSource_CheckedChanged;
+
+            lst_bckp_rdBtn.CheckedChanged +=
+                BackupSource_CheckedChanged;
+
+            othr_rdBtn.CheckedChanged +=
+                BackupSource_CheckedChanged;
+
+
+            sbmt_btn.Click += sbmt_btn_Click;
+
+            cncl_btn.Click += cncl_btn_Click;
         }
+
 
         private void Form2_Load(object sender, EventArgs e)
         {
-
         }
 
-        public void button1_Click(object sender, EventArgs e)
+        private void BackupSource_CheckedChanged(object sender,EventArgs e)
         {
+
+            if (othr_rdBtn.Checked)
+            {
+                bckp_dt_pckr.Enabled = true;
+            }
+            else
+            {
+                bckp_dt_pckr.Enabled = false;
+            }
+        }
+
+
+        private void sbmt_btn_Click(
+            object sender,
+            EventArgs e)
+        {
+            if (cmpnydb_cmbBx.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    "Please select a company database.",
+                    "Missing Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+
+
+            string requestName =
+                rstr_rq_nm_txtbx.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(requestName))
+            {
+                MessageBox.Show(
+                    "Please enter a restore request name.",
+                    "Missing Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                rstr_rq_nm_txtbx.Focus();
+
+                return;
+            }
+
+
+
+            string backupSource = "";
+
+            if (frsh_bckp_rdBtn.Checked)
+            {
+                backupSource = "Fresh Backup";
+            }
+            else if (lst_bckp_rdBtn.Checked)
+            {
+                backupSource = "Last Backup";
+            }
+            else if (othr_rdBtn.Checked)
+            {
+                backupSource = "Other";
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Please select a backup source.",
+                    "Missing Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+
+            string databaseName =
+                cmpnydb_cmbBx.SelectedItem.ToString();
+
+
+
+
+            string backupDate = "";
+
+            if (othr_rdBtn.Checked)
+            {
+                backupDate =
+                    bckp_dt_pckr.Value.ToString("dd/MM/yyyy");
+            }
+
+
+
+            string message =
+                "Restore request created successfully!\n\n" +
+                "Company Database: " + databaseName + "\n" +
+                "Request Name: " + requestName + "\n" +
+                "Virtual Machine: " + virtualMachine + "\n" +
+                "Restore Source: " + backupSource;
+
+
+            // Only show the date when "Other" was selected.
+            if (othr_rdBtn.Checked)
+            {
+                message +=
+                    "\nBackup Date: " + backupDate;
+            }
+
+
+            MessageBox.Show(
+                message,
+                "Restore Request",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+
+
+          
+
+        private void cncl_btn_Click(
+            object sender,
+            EventArgs e)
+        {
+            // Clear database selection.
+            cmpnydb_cmbBx.SelectedIndex = -1;
+
+            // Clear request name.
+            rstr_rq_nm_txtbx.Clear();
+
+            // Clear restore source.
+            frsh_bckp_rdBtn.Checked = false;
+            lst_bckp_rdBtn.Checked = false;
+            othr_rdBtn.Checked = false;
+
+            // Disable date picker again.
+            bckp_dt_pckr.Enabled = false;
+        }
+
+
+
+        public void bckp_rq_lgout_btn_Click(
+            object sender,
+            EventArgs e)
+        {
+            // Create the login form again.
             Form1 nextForm = new Form1();
 
-            // Display Form2
+            // Show login form.
             nextForm.Show();
 
-            // Hide the current Form1
+            // Hide the current form.
             this.Hide();
         }
     }
